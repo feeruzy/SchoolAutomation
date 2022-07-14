187b4741228593e0d9e42271dd3772413348c509
feeruzy <feeruzy@gmail.com>
2022-07-14 22:43:15 +0430

    update: mifrations for db changes.

---
+++ Core/Migrations/20220714181247_property name.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Core.Migrations
+{
+    public partial class propertyname : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Titr",
+                table: "tblDisciplines",
+                newName: "Title");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "KasreNomre",
+                table: "tblDisciplines",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "tblDisciplines",
+                newName: "Titr");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "KasreNomre",
+                table: "tblDisciplines",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+    }
+}
