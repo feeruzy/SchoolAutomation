e997d02adc548fd72f41dd458a2856786243c4ce
nasrinkaveh <n.nasrinkaveh@gmail.com>
2022-07-17 16:47:13 +0430

    add: core: Edit ClassYear & ClassDiscipline

--- Core/Helper/ClassDiciplineManagement.cs
+++ Core/Helper/ClassDiciplineManagement.cs
@@ -2,38 +2,53 @@ namespace School.Core;
 
 public class DisciplineManagement
 {
-  private readonly ModelDb _db = new ModelDb();
+    private readonly ModelDb _db = new ModelDb();
 
-  public IEnumerable<ClassDiscipline> List()
-  {
-    foreach (var item in _db.tblDisciplines)
-      yield return item;
-  }
-
-  public int CreateDisciplineItem(string title, float score)
-  {
-    if (_db.tblDisciplines.Any(d => d.Title == title))
-      throw new ApplicationException("عنوان تکراری است");
-
-    ClassDiscipline newItem = new ClassDiscipline()
+    public IEnumerable<ClassDiscipline> List()
     {
-      KasreNomre = score,
-      Title = title
-    };
+        foreach (var item in _db.tblDisciplines)
+            yield return item;
+    }
 
-    _db.tblDisciplines.Add(newItem);
-    _db.SaveChanges();
-    return newItem.Id;
-  }
+    public int CreateDisciplineItem(string title, float score)
+    {
+        if (_db.tblDisciplines.Any(d => d.Title == title))
+            throw new ApplicationException("عنوان تکراری است");
 
-  public void EditDisciplineItem() {
-    // todo: check this.
-    // tip: check if item found then select it and update its properties.
-  }
+        ClassDiscipline newItem = new ClassDiscipline()
+        {
+            KasreNomre = score,
+            Title = title
+        };
 
-  public void RemoveDisciplineItem() {
-    // todo: check this.
-    // tip: first check if discipline item used in discipline student table.
-    // if not used it can be remove.
-  }
+        _db.tblDisciplines.Add(newItem);
+        _db.SaveChanges();
+        return newItem.Id;
+    }
+
+    public void EditDisciplineItem(int id, string newTitle, float newKasreNomre)
+    {
+
+        if (!_db.tblDisciplines.Any(i => i.Id == id))
+            throw new ApplicationException("مورد انظباطی یافت نشد");
+
+        var item = _db.tblDisciplines.FirstOrDefault(i => i.Id == id);
+        item.Title = newTitle;
+        item.KasreNomre = newKasreNomre;
+        _db.SaveChanges();
+
+    }
+
+    public void RemoveDisciplineItem(int id)
+    {
+        if (!_db.tblDisciplines.Any(i => i.Id == id))
+            throw new ApplicationException("کد مورد انضباطی نادرست است.");
+
+        if (_db.tblDisciplineStudents.Any(i => i.Fk_DiciplineId == id))
+            throw new ApplicationException("مورد انضباطی قبلا استفاده شده و قابل حذف نمی باشد.");
+
+        var item = _db.tblDisciplines.FirstOrDefault(i => i.Id == id);
+        _db.tblDisciplines.Remove(item);
+        _db.SaveChanges();
+    }
 }
--- Core/Helper/ClassManagementYear.cs
+++ Core/Helper/ClassManagementYear.cs
@@ -5,55 +5,69 @@ namespace School.Core;
 /// </summary>
 public class YearManager
 {
-  private readonly ModelDb _db = new ModelDb();
+    private readonly ModelDb _db = new ModelDb();
 
-  /// <summary>
-  /// لیست سال های تحصیلی ثبت شده در سیستم
-  /// </summary>
-  /// <returns></returns>
-  public IEnumerable<ClassYear> ListYears()
-  {
-    foreach (var item in _db.tblYears)
-      yield return item;
-  }
-
-
-  /// <summary>
-  /// تعریف سال جدید
-  /// </summary>
-  /// <param name="name">نام سال تحصیلی </param>
-  /// <returns>شماره ایدی سال جدید</returns>
-  public int CreateYear(string name)
-  {
-    if (_db.tblYears.Any(i => i.Name == name))
-      throw new ApplicationException("نام سال تکراری است.");
-
-    var newYear = new ClassYear()
+    /// <summary>
+    /// لیست سال های تحصیلی ثبت شده در سیستم
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerable<ClassYear> ListYears()
     {
-      Name = name,
-      IsActive = false
-    };
-    _db.tblYears.Add(newYear);
-    _db.SaveChanges();
+        foreach (var item in _db.tblYears)
+            yield return item;
+    }
 
-    return newYear.Id;
-  }
 
-  /// <summary>
-  ///     activate selected year.
-  /// </summary>
-  /// <param name="id">year id</param>
-  public void EditActivation(int id)
-  {
-    if (!_db.tblYears.Any(i => i.Id == id))
-      throw new ApplicationException("سال تحصیلی مورد نظر یافت نشد.");
+    /// <summary>
+    /// تعریف سال جدید
+    /// </summary>
+    /// <param name="name">نام سال تحصیلی </param>
+    /// <returns>شماره ایدی سال جدید</returns>
+    public int CreateYear(string name)
+    {
+        if (_db.tblYears.Any(i => i.Name == name))
+            throw new ApplicationException("نام سال تکراری است.");
 
-    foreach (var y in _db.tblYears) y.IsActive = y.Id == id;
-    _db.SaveChanges();
-  }
+        var newYear = new ClassYear()
+        {
+            Name = name,
+            IsActive = false
+        };
+        _db.tblYears.Add(newYear);
+        _db.SaveChanges();
 
-  public void removeYear(int id) {
-    // todo: check this.
-  }
+        return newYear.Id;
+    }
+
+    /// <summary>
+    ///     activate selected year.
+    /// </summary>
+    /// <param name="id">year id</param>
+    public void EditActivation(int id)
+    {
+        if (!_db.tblYears.Any(i => i.Id == id))
+            throw new ApplicationException("سال تحصیلی مورد نظر یافت نشد.");
+
+        foreach (var y in _db.tblYears) y.IsActive = y.Id == id;
+        _db.SaveChanges();
+    }
+
+    public void removeYear(int id)
+    {
+        if (!_db.tblYears.Any(i => i.Id == id))
+            throw new ApplicationException("کد سال تحصیلی در سیستم وجود ندارد.");
+
+        if (_db.tblDisciplineStudents.Any(i => i.Fk_YearId == id))
+            throw new ApplicationException("سال تحصیلی منتخب شما در سایر جداول دارای اطلاعات است و قابل حذف نیست.");
+
+        var item = _db.tblYears.FirstOrDefault(i => i.Id == id);
+
+        if (item.IsActive)
+            throw new ApplicationException("سال تحصیلی فعال را نمی توان از سیستم حذف کرد.");
+
+        _db.tblYears.Remove(item);
+        _db.SaveChanges();
+
+    }
 
 }
