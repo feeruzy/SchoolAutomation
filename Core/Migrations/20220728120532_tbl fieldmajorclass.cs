fb0f86a69f8554415a7af7cc21c69fa7dbc2a7dd
nasrinkaveh <n.nasrinkaveh@gmail.com>
2022-07-28 16:37:16 +0430

    add: core: migratins

---
+++ Core/Migrations/20220728120532_tbl fieldmajorclass.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Core.Migrations
+{
+    public partial class tblfieldmajorclass : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tblClasses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Fk_Major = table.Column<int>(type: "int", nullable: false),
+                    FK_Field = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblClasses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tblFields",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblFields", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tblMajors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblMajors", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tblClasses");
+
+            migrationBuilder.DropTable(
+                name: "tblFields");
+
+            migrationBuilder.DropTable(
+                name: "tblMajors");
+        }
+    }
+}
